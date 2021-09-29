7aac5f0ca7121fa2490e3a972a29f277d49b17d7
Muhammad Raza Khan <muhammadraza9763@gmail.com>
2021-09-29 12:50:50 +0530

    Add slider instead of button to take input

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-const inputBtn = document.querySelector(".input-btn");
+// const inputBtn = document.querySelector(".input-btn");
 const gridContainer = document.querySelector(".grid-container");
-
-let squareNo = 16;
-
+const slider = document.querySelector("input[type=range]");
+const gridSizeInfo = document.querySelector(".grid-size");
+let sideSquareNumber = 16;
+slider.value = 16;
 // Function to generate divs based on user input of number of squares
-function generateGridDivs(squareNo) {
+function generateGridDivs(sideSquareNumber) {
   let div = "";
-  for (let i = 0; i < squareNo * squareNo; i++) {
+  for (let i = 0; i < sideSquareNumber * sideSquareNumber; i++) {
     div += `<div class="grid-item"></div>`;
   }
   gridContainer.innerHTML += div;
   changeBoxHoverColor();
 }
-generateGridDivs(squareNo);
+generateGridDivs(sideSquareNumber);
 
 // Function changing each individual box's background-color to a random color
-function changeBoxHoverColor() {
+function changeBoxHoverColor(color) {
   const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((item) => {
     item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = generateRandomColor();
+      color = generateRandomColor();
+      item.style.backgroundColor = color;
     });
   });
 }
 
 // Function to get number of squares of each side from user and then generate grid
-inputBtn.addEventListener("click", () => {
-  squareNo = prompt("Enter the number of squares per side", "");
-  if (squareNo > 100) {
-    squareNo = prompt("Too many boxes, please specify a number less than 100");
-  }
+slider.addEventListener("input", () => {
+  gridSizeInfo.textContent = `${slider.value} by ${slider.value}`;
+  sideSquareNumber = slider.value;
   while (gridContainer.firstChild) {
     gridContainer.firstChild.remove();
   }
-  generateGridDivs(squareNo);
-  gridContainer.style.gridTemplateColumns = `repeat(${squareNo},1fr)`;
-  gridContainer.style.gridTemplateRows = `repeat(${squareNo},1fr)`;
+  generateGridDivs(sideSquareNumber);
+  gridContainer.style.gridTemplateColumns = `repeat(${sideSquareNumber},1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${sideSquareNumber},1fr)`;
 });
 
 // Function to return a random hex color
@@ -55,4 +55,10 @@ clearButton.addEventListener("click", () => {
   gridItems.forEach((item) => {
     item.style.backgroundColor = "#fff";
   });
+});
+
+const eraserButton = document.querySelector(".eraser-btn");
+eraserButton.addEventListener("click", () => {
+  color = "#fff";
+  changeBoxHoverColor(color);
 });
